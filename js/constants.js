eb7caba7aa73d760cd9794d1185be805ec02f8bf
apal21 <apalshah21121996@gmail.com>
2018-11-15 21:29:21 +0530

    replaced http with https

--- js/constants.js
+++ js/constants.js
@@ -2,7 +2,7 @@ const center = [12.9716, 77.5946]; // Bengaluru
 const zoom = 12;
 
 // Before Map URL
-const map1URL = 'http://{s}.tiles.mapbox.com/v3/spatial.map-qgihrqg5/{z}/{x}/{y}.png';
+const map1URL = 'https://{s}.tiles.mapbox.com/v3/spatial.map-qgihrqg5/{z}/{x}/{y}.png';
 
 // After Map URL
 const map2URL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
